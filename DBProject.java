62f4d8be399bf12d2774cb9b1aaf3a9a8bcfd19a
Abdullah Majmundar <amajm001@sledge.cs.ucr.edu>
2019-12-05 21:58:39 -0800

    Project 1-7

--- DBProject.java
+++ DBProject.java
@@ -91,17 +91,18 @@ public class DBProject {
     * @return the number of rows returned
     * @throws java.sql.SQLException when failed to execute the query
     */
-   public int executeQuery (String query) throws SQLException {
+   public ResultSet executeQuery (String query) throws SQLException {
       // creates a statement object
       Statement stmt = this._connection.createStatement ();
 
       // issues the query instruction
       ResultSet rs = stmt.executeQuery (query);
-
+      return rs;
       /*
        ** obtains the metadata object for the returned result set.  The metadata
        ** contains row and column info.
        */
+      /*
       ResultSetMetaData rsmd = rs.getMetaData ();
       int numCol = rsmd.getColumnCount ();
       int rowCount = 0;
@@ -121,8 +122,9 @@ public class DBProject {
          System.out.println ();
          ++rowCount;
       }//end while
-      stmt.close ();
-      return rowCount;
+      //stmt.close ();
+      return rs;
+*/
    }//end executeQuery
 
    /**
@@ -254,51 +256,265 @@ public class DBProject {
 
    
    public static void addCustomer(DBProject esql){
-	  // Given customer details add the customer in the DB 
-      // Your code goes here.
-      // ...
-      // ...
+	  // Given customer details add the customer in the DB
+	try {
+		ResultSet c = esql.executeQuery("SELECT MAX(customerID) AS max_id FROM customer");
+		int cID = -1;
+		if (c.next()) {
+                	cID = c.getInt("max_id") + 1;
+		}
+		System.out.print("\tEnter your First Name: ");
+		String fname = in.readLine();
+		System.out.print("\tEnter your Last Name: ");
+		String lname = in.readLine();
+		System.out.print("\tEnter your Address: ");
+		String Address = in.readLine();
+		System.out.print("\tEnter your Phone Number with no symbols or spaces: ");
+		String phNo = in.readLine();
+		System.out.print("\tEnter your Date of Birth using YYYY-MM-DD format: ");
+		String DOB = in.readLine();
+		System.out.print("\tEnter your Gender: ");
+		String gender = in.readLine();
+		try{
+			String query = "INSERT INTO customer (customerID, fname, lname, address, phno, dob, gender) VALUES (" + cID + ", \'" + fname + "\', \'" + lname + "\', \'" + Address + "\', " + phNo + ", DATE \'" + DOB + "\', \'" + gender +"\')";
+			esql.executeQuery(query);
+			System.out.print("Successfully Added Customer!");
+		}catch(Exception e) {
+			System.err.println (e.getMessage());
+		}
+	} catch (Exception e){
+		System.err.println (e.getMessage());
+	}
    }//end addCustomer
 
    public static void addRoom(DBProject esql){
 	  // Given room details add the room in the DB
-      // Your code goes here.
-      // ...
-      // ...
+	try{
+		System.out.print("\tEnter the Hotel ID: ");
+		String hID = in.readLine();
+		String getHID = "SELECT * FROM Hotel WHERE hotelID = " + hID;
+		ResultSet res = esql.executeQuery(getHID);
+		if (!res.isBeforeFirst()) {
+			System.out.print("\tHotel ID Not Found\n");
+		} else {
+			System.out.print("\tEnter the Room Number: ");
+			String roomNum = in.readLine();
+			ResultSet res1 = esql.executeQuery("Select * FROM Room WHERE hotelID = " + hID + " AND roomNo = " + roomNum);
+			if (res1.next()) {
+				System.out.print("\tRoom Already Exists\n");
+			} else{
+				System.out.print("\tEnter the Room Type: ");
+				String rType = in.readLine();
+				String query = "INSERT INTO Room (hotelID, roomNo, roomType) VALUES (" + hID + ", " + roomNum + ", \'" + rType + "\')";
+				esql.executeQuery(query);
+				System.out.print("\tSuccessfully Added Room!\n");
+			}
+		}
+	} catch (Exception e) {
+		System.err.println (e.getMessage());
+	}
    }//end addRoom
 
    public static void addMaintenanceCompany(DBProject esql){
       // Given maintenance Company details add the maintenance company in the DB
-      // ...
-      // ...
+	try {
+		System.out.print("\tEnter the Company Name: ");
+		String CName = in.readLine();
+		System.out.print("\tEnter the Company Address: ");
+		String cAddress = in.readLine();
+		String check = "SELECT * FROM MaintenanceCompany WHERE name = \'" + CName + "\'  AND address = \'" + cAddress + "\'";
+		ResultSet res = esql.executeQuery(check);
+		if (res.next()) {
+			System.out.print("\tCompany Already Exists in Database\n");
+		} else {
+			System.out.print("\tIs the Company Certified (Y/N): ");
+			String cert = in.readLine();
+			String cmp = new String("Y");
+			boolean certBool = false;
+			if (cert.equals(cmp)) {
+				certBool = true;
+			}
+			ResultSet c = esql.executeQuery("SELECT MAX(cmpID) AS max_id FROM MaintenanceCompany");
+                	int cID = -1;
+			if (c.next()) {
+				cID = c.getInt("max_id") + 1;
+			}
+			String query = "INSERT INTO MaintenanceCompany (cmpID, name, address, isCertified) VALUES (" + cID + ", \'" + CName + "\', \'" + cAddress + "\', " + certBool +")";
+			esql.executeQuery(query);
+			System.out.print("\tSuccessfully Added a Maintenance Company!\n");
+		}
+	} catch (Exception e) {
+		System.err.println (e.getMessage());
+	}
    }//end addMaintenanceCompany
 
    public static void addRepair(DBProject esql){
 	  // Given repair details add repair in the DB
-      // Your code goes here.
-      // ...
-      // ...
+	try {
+		System.out.print("\tEnter the Hotel ID: ");
+		String hID = in.readLine();
+		System.out.print("\tEnter the Room Number: ");
+		String roomNum = in.readLine();
+		ResultSet res = esql.executeQuery("Select * FROM Room WHERE hotelID = " + hID + " AND roomNo = " + roomNum);
+		if (res.next()) {
+			System.out.print("\tEnter the Maintenace Company ID: ");
+			String maintID = in.readLine();
+			ResultSet res1 = esql.executeQuery("SELECT * FROM MaintenanceCompany WHERE cmpID = " + maintID + " AND isCertified");
+			if (res1.next()) {
+				ResultSet c = esql.executeQuery("SELECT MAX(rID) AS max_id FROM Repair");
+                        	int rID = -1;
+                        	if (c.next()) {
+                                	rID = c.getInt("max_id") + 1;
+                        	}
+				System.out.print("\tEnter the Repair Date in YYYY-MM-DD Format: ");
+				String date = in.readLine();
+				System.out.print("\tEnter a Description of the maintenance required: ");
+				String desc = in.readLine();
+				System.out.print("\tEnter the Repair Type: ");
+				String rType = in.readLine();
+				String query = "INSERT INTO Repair (rID, hotelID, roomNo, mCompany, repairDate, description, repairType) VALUES (" + rID + ", " + hID + ", " + roomNum + ", " + maintID + ", DATE \'" + date + "\', \'" + desc + "\', \'" + rType + "\')";
+				esql.executeQuery(query);
+				System.out.print("Successfully Added a Maintenance Request");
+			} else {
+				System.out.print("\tMaintenance Company Does Not Exist!\n");
+			}
+		} else {
+			System.out.print("\tNo Such Room Exists!\n");
+		}
+	} catch (Exception e) {
+		System.err.println(e.getMessage());
+	}
    }//end addRepair
 
    public static void bookRoom(DBProject esql){
 	  // Given hotelID, roomNo and customer Name create a booking in the DB 
-      // Your code goes here.
-      // ...
-      // ...
+	try {
+		System.out.print("\tEnter Hotel ID: ");
+		String hID = in.readLine();
+		System.out.print("\tEnter the Room Number: ");
+		String roomNum = in.readLine();
+		ResultSet res = esql.executeQuery("Select * FROM Room WHERE hotelID = " + hID + " AND roomNo = " + roomNum);
+		if (res.next()) {
+			System.out.print("\tEnter the Customer's First Name: ");
+			String Fname = in.readLine();
+			System.out.print("\tEnter the Customer's Last Name: ");
+			String Lname = in.readLine();
+			ResultSet getCust = esql.executeQuery("SELECT COUNT(*) AS rowcount FROM customer WHERE fname = \'" + Fname + "\' AND lname = \'" + Lname + "\'");
+			if (getCust.next()) {
+				int count = getCust.getInt("rowcount");
+				String cID = "";
+				ResultSet getCID = esql.executeQuery("SELECT * FROM customer WHERE fname = \'" + Fname + "\' AND lname = \'" + Lname + "\'");
+				if (count == 1) {
+					getCID.next();
+					cID = getCID.getString("customerID");
+				} else {
+					System.out.print("\tThere are more than 1 customer with this name. Please specify a Customer ID: ");
+					cID = in.readLine();
+				}
+				ResultSet verify = esql.executeQuery("SELECT * FROM customer WHERE customerID = " + cID);
+				if (verify.next()) {
+					System.out.print("\tEnter the Date you with to book the room for in YYYY-MM-DD Format: ");
+					String date = in.readLine();
+					ResultSet checkDate = esql.executeQuery("SELECT * FROM Booking WHERE hotelID = " + hID + " AND roomNo = " + roomNum + " AND bookingDate = DATE \'" + date + "\'");
+					if (!checkDate.next()) {
+						ResultSet c = esql.executeQuery("SELECT MAX(bID) AS max_id FROM Booking");
+                                		int bID = -1;
+                                		if (c.next()) {
+                                        		bID = c.getInt("max_id") + 1;
+                                		}
+						System.out.print("Enter the Price of the room: ");
+						String price = in.readLine();
+						System.out.print("Enter the number of Occupants: ");
+						String numPeople = in.readLine();
+						String query = "INSERT INTO Booking (bID, customer, hotelID, roomNo, bookingDate, noOfPeople, price) VALUES (" + bID + ", " + cID + ", " + hID + ", " + roomNum + ", DATE \'" + date + "\', " + numPeople + ", "+ price + ")";
+						esql.executeQuery(query);
+						System.out.print("Successfully Booked Room!");
+					} else {
+						System.out.print("\tRoom is already Booked\n");
+					}
+				} else {
+					System.out.print("\tInvalid Customer ID!\n");
+				}
+			} else {
+				System.out.print("\tNo Customer with that name exists!\n");
+			}
+		} else {
+			System.out.print("\tNo Such Room Exists!\n");
+		}
+	} catch (Exception e) {
+		System.err.println(e.getMessage());
+	}
    }//end bookRoom
 
    public static void assignHouseCleaningToRoom(DBProject esql){
 	  // Given Staff SSN, HotelID, roomNo Assign the staff to the room 
-      // Your code goes here.
-      // ...
-      // ...
+	try {
+		System.out.print("\tEnter the Staff's Social Security Number: ");
+		String ssn = in.readLine();
+		System.out.print("\tEnter the Hotel ID: ");
+		String hID = in.readLine();
+		ResultSet staff = esql.executeQuery("SELECT * FROM Staff WHERE SSN = " + ssn + " AND employerID = " + hID + " AND role = \'HouseCleaning\'");
+		String fname = "";
+		if (staff.next()) {
+			fname = staff.getString("fname");
+			System.out.print("\tEnter the Room Number you wish to asign to " + fname + ": ");
+			String roomNum = in.readLine();
+			ResultSet res = esql.executeQuery("Select * FROM Room WHERE hotelID = " + hID + " AND roomNo = " + roomNum);
+                	if (res.next()) {
+				 ResultSet c = esql.executeQuery("SELECT MAX(asgID) AS max_id FROM Assigned");
+                                 int asgID = -1;
+                                 if (c.next()) {
+                                 	asgID = c.getInt("max_id") + 1;
+                                 }
+				String query = "INSERT INTO Assigned (asgID, staffID, hotelID, roomNo) VALUES (" + asgID + ", " + ssn + ", " + hID + ", " + roomNum + ")";
+				System.out.print("\tSuccessfully Assigned " + fname + " to Room Number " + roomNum + "!\n");
+			} else {
+				System.out.print("\tRoom does not exist in the Hotel!\n");
+			}
+		} else {
+			System.out.print("\tStaff Member Does not exist!\n");
+		}
+	} catch (Exception e) {
+		System.err.println(e.getMessage());
+	}
    }//end assignHouseCleaningToRoom
    
    public static void repairRequest(DBProject esql){
 	  // Given a hotelID, Staff SSN, roomNo, repairID , date create a repair request in the DB
-      // Your code goes here.
-      // ...
-      // ...
+	try {
+		System.out.print("\tEnter the Hotel ID: ");
+		String hID = in.readLine();
+		System.out.print("\tEnter your Social Security Number: ");
+		String ssn = in.readLine();
+		ResultSet checkHotelManager = esql.executeQuery("SELECT * FROM Hotel WHERE hotelID = " + hID + " AND manager = " + ssn);
+		if (checkHotelManager.next()) {
+			System.out.print("\tEnter the Room Number: ");
+			String roomNum = in.readLine();
+			System.out.print("\tEnter the Repair ID: ");
+			String repID = in.readLine();
+			ResultSet checkRepair = esql.executeQuery("SELECT * FROM Repair WHERE rID =" + repID + " AND hotelID = " + hID + " AND roomNo = " + roomNum);
+			if (checkRepair.next()) {
+				ResultSet c = esql.executeQuery("SELECT MAX(reqID) AS max_id FROM Request");
+                		int rID = -1;
+                		if (c.next()) {
+                        		rID = c.getInt("max_id") + 1;
+                		}
+				System.out.print("\tEnter Today's Date in YYYY-MM-DD Format: ");
+				String date = in.readLine();
+				System.out.print("\tEnter a Description of the Repair Request: ");
+				String desc = in.readLine();
+				String query = "INSERT INTO Request (reqID, managerID, repairID, requestDate, description) VALUES (" + rID + ", " + ssn + ", " + repID + ", DATE \'" + date + "\', \'" + desc +"\')";
+				esql.executeQuery(query);
+				System.out.print("\tSuccessfully Made a Repair Request!\n");
+			} else {
+				System.out.print("\tNot a Valid Repair, Hotel, Room Number Combination!\n");
+			}
+		} else {
+			System.out.print("\tNot a valid user!\n");
+		}
+	} catch (Exception e) {
+		System.err.println(e.getMessage());
+	}
    }//end repairRequest
    
    public static void numberOfAvailableRooms(DBProject esql){
